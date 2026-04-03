0e733d36f21df7dae670fd0b47b97b59a2a2ce0f
Ingyin-Phyo275 <xiluhan.igp@gmail.com>
2026-04-03 13:57:46 +0630

    Remove server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,21 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-    // base: '/creator-portal/',
+    base: '/creator-portal/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    proxy: {
-      // When the frontend calls "/api", Vite redirects it
-      '/api': {
-        target: 'https://uat.api.amuze.com.mm/api/', // Your real backend
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     // When the frontend calls "/api", Vite redirects it
+  //     '/api': {
+  //       target: 'https://uat.api.amuze.com.mm/api/', // Your real backend
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     },
+  //   },
+  // },
 })

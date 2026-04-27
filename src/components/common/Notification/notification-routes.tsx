3aea7e7964fcaf258f8aaaf3f046a01dd593fd57
PhomeMyintMo <phomemyintmo82@gmail.com>
2026-04-27 16:59:05 +0630

    some changes in noti

--- src/components/common/Notification/notification-routes.tsx
+++ src/components/common/Notification/notification-routes.tsx
@@ -1,37 +1,49 @@
 export const notificationRoutes: Record<string, (data: any) => string> = {
   // Novel
   "USER LIKED NOVEL": (d) => `/entertainment/novel/details/${d.titleId}`,
-  "USER COMMENT ON NOVEL": (d) => `/entertainment/novel/details/${d.titleId}`,
+  "USER COMMENT ON NOVEL": (d) => ` /entertainment/novel/details/${d.titleId}`,
+  "USER RATED NOVEL": (d) => `/entertainment/novel/details/${d.titleId}`,
 
   // Comic
   "USER LIKED COMIC": (d) => `/entertainment/comics/details/${d.titleId}`,
   "USER COMMENT ON COMIC": (d) => `/entertainment/comics/details/${d.titleId}`,
+  "USER RATED COMIC": (d) => `/entertainment/comics/details/${d.titleId}`,
+
 
   // Story
   "USER LIKED STORY": (d) => `/entertainment/storytelling/details/${d.titleId}`,
   "USER COMMENT ON STORY": (d) =>
     `/entertainment/storytelling/details/${d.titleId}`,
+    "USER RATED STORY": (d) => `/entertainment/storytelling/details/${d.titleId}`,
+
 
   // Gallery
   "USER LIKED GALLERY": (d) => `/entertainment/gallery/details/${d.titleId}`,
   "USER COMMENT ON GALLERY": (d) =>
     `/entertainment/gallery/details/${d.titleId}`,
+  "USER RATED GALLERY": (d) => `/entertainment/gallery/details/${d.titleId}`,
+
 
   // MuzeBox
-  "USER LIKED MUZEBOX": (d) => `/entertainment/muzebox/details/${d.titleId}`,
+  "USER LIKED MUZEBOX": (d) => `/entertainment/muze-box/details/${d.titleId}`,
   "USER COMMENT ON MUZEBOX": (d) =>
     `/entertainment/muzebox/details/${d.titleId}`,
+    "USER RATED MUZEBOX": (d) => `/entertainment/muze-box/details/${d.titleId}`,
+
 
   // Education
   "USER LIKED EDUCATION": (d) =>
     `/entertainment/educatin/grades/details/${d.titleId}`,
 
+
   // Museum
   "USER LIKED MUSEUM": (d) => `/entertainment/museum/details/${d.titleId}`,
+
 
   // Post
   "USER LIKED POST": (d) => `/entertainment/posts/details/${d.titleId}`,
   "USER COMMENT ON POST": (d) => `/entertainment/posts/details/${d.titleId}`,
+
 };
 
 export const getNotificationRoute = (type: string, data: any): string => {
